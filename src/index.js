d8efec19e8c2d6b74a447209c872cabc92cf05eb
Garron Ma <garronma@gmail.com>
2019-05-26 16:22:38 -0700

    [05/26/19] Setup linter/webpack configs

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootDivElement = document.querySelector('#root');
+
+ReactDOM.render(<h1>Lets get this going now</h1>, rootDivElement);
---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,37 @@
+const PATH = require('path');
+// const HTMLWebpack = require('html-webpack-plugin');
+
+// const HTMLWebpackPlugin = new HTMLWebpack({
+//   template: PATH.join(__dirname, '../src/index.html'),
+//   filename: 'index.html'
+// });
+
+module.exports = {
+  entry: PATH.join(__dirname, '../src/index.js'),
+  output: {
+    path: PATH.join(__dirname, '../dist'),
+    filename: 'bundle.js'
+  },
+  // devServer: {
+  //   // compress: true,
+  //   contentBase: PATH.join(__dirname, 'dist'),
+  //   // historyApiFallback: true,
+  //   hot: true,
+  //   open: true,
+  //   publicPath: '/'
+  // },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  // plugins: [HTMLWebpackPlugin],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
